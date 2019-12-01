fc4eb9d43b147a10fee57af06b481f229e9c483f
whitegreyblack <sangwoowhang@gmail.com>
2019-11-30 16:11:43 -0800

    Modifying some variable names

--- source/screens/death_screen.py
+++ source/screens/death_screen.py
@@ -18,14 +18,13 @@ class DeathScreen(Screen):
     def render(self):
         x = self.engine.width // 2
         y = self.engine.height // 2
-        string = "you died"
+        message = "you died"
 
         self.terminal.erase()
-        border(self.terminal, 0, 0, self.engine.width-1, self.engine.height-1)
+        border(self.terminal, 0, 0, self.engine.width - 1, self.engine.height - 1)
         self.terminal.addstr(0, 1, '[death screen]')
-        self.terminal.addstr(y, x-len(string)//2, string)
+        self.terminal.addstr(y, x - len(message) // 2, message)
         self.terminal.refresh()
     
     def handle_input(self):
         self.engine.running = False
-        # self.engine.change_screen('mainmenu')
